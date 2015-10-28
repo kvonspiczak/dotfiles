cb142d188e5e40b14da83d11d420264cb228488d
Kevin von Spiczak <kvonspiczak@q4u.de>
2015-10-28 11:47:02 +0100

    remove ctf tools

--- brew.sh
+++ brew.sh
@@ -54,32 +54,6 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-# brew install aircrack-ng
-# brew install bfg
-# brew install binutils
-# brew install binwalk
-# brew install cifer
-# brew install dex2jar
-# brew install dns2tcp
-# brew install fcrackzip
-# brew install foremost
-# brew install hashpump
-# brew install hydra
-# brew install john
-# brew install knock
-# brew install netpbm
-# brew install nmap
-# brew install pngcheck
-# brew install socat
-# brew install sqlmap
-# brew install tcpflow
-# brew install tcpreplay
-# brew install tcptrace
-# brew install ucspi-tcp # `tcpserver` etc.
-# brew install xpdf
-# brew install xz
-
 # Install other useful binaries.
 brew install ack
 brew install dark-mode
@@ -99,6 +73,9 @@ brew install ssh-copy-id
 brew install tree
 brew install webkit2png
 brew install zopfli
+
+# Install Brewfile
+brew bundle
 
 # Remove outdated versions from the cellar.
 brew cleanup
